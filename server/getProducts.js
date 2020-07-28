e5cbaa567bd2207884a387a16234b58b77693f77
karenChernandez <karenChernandez@live.com>
2020-07-27 17:18:20 -0700

    completed

---
+++ server/getProducts.js
@@ -0,0 +1,19 @@
+const products = require('../products.json');
+
+
+
+const getProducts =(req, res)=>{
+    if(req.query.price){
+        console.log(req.query)
+        const items= products.filter(val=>val.price >= parseInt(req.query.price));
+        return res.status(200).send(items);
+    }
+    res.status(200).send(products)
+
+}
+
+
+
+
+
+module.exports = getProducts;
